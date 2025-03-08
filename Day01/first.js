8fc5c3f6906c329fe5e59ee628120672e1fb151d
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-03-09 00:53:45 +0530

    starting with backend

---
+++ Day01/first.js
@@ -0,0 +1,23 @@
+const {sum,sub}=require("./second")
+
+
+
+//require format (IIFE)
+// (
+//     function (){
+//         console.log("I am Second")
+
+//         function sum(a,b){
+//             console.log(a+b);
+//         }
+//     })();
+
+
+
+sum(3,4)
+sub(7,8)
+
+console.log("Hello I am First");
+
+
+//CJS:- common javascript module system
---
+++ Day01/sour/first.js
@@ -0,0 +1,4 @@
+import sum from "./second.js";
+
+sum(3,8);
+console.log("Hello ji");
