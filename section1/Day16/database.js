39060e7e2e54059aa8d1367b07b8d5932475dcc6
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-04-15 19:04:10 +0530

    pauseing

--- section1/Day16/database.js
+++ section1/Day16/database.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+async function main(params) {
+    await mongoose.connect("mongodb+srv://RohitSah45:Rohit%40450987@codingadda.9k3dsmx.mongodb.net/")
+}
+
+main().then(()=>console.log("Connected to DB")).catch((err)=>console.log(err)
+)
