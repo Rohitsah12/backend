6a193c823a1975a550364004d3e2eed2b60251da
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-04-16 23:48:54 +0530

    learned data validation in mongoose

--- section1/Day17/index.js
+++ section1/Day17/index.js
@@ -4,30 +4,57 @@ const main=require("./database")
 const User=require("./models/users")
 app.use(express.json());
 
-app.get("/info",async (req,res)=>{
-    const ans=await User.find({});
-    res.send(ans);
-})
-
-app.post("/info",async (req,res)=>{
-
-    // const ans=new User(req.body);
-    // await ans.save();
-    // res.send("successfully updates")
-
+app.post("/register",async (req,res)=>{
     try{
         await User.create(req.body);
-        res.send("Successfully Updated");
+        res.send("user Registered Successfully")
     }
     catch(err){
-        res.send(err);
+        res.send("Error"+err);
     }
 })
 
-app.delete("/info",async (req,res)=>{
-    await User.deleteOne({name:"Vishal"})
-    res.send("Deleted")
+app.get("/info",async (req,res)=>{
+    try{
+        const result=await User.find();
+        res.send(result);
+    }
+    catch(err){
+        res.send("Error: "+err);
+    }
 })
+
+app.get("/user/:id",async (req,res)=>{
+    try{
+        const result=await User.findById(req.params.id);
+        res.send(result);
+    }
+    catch(err){
+        res.send("Error:"+err);
+    }
+})
+
+app.delete("/user/:id",async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.send("Deleted Successfully")
+    }
+    catch(err){
+        res.send("Error:"+err);
+    }
+})
+
+app.patch("/user",async (req,res)=>{
+    try {
+        const {_id,...update}=req.body;
+        await User.findByIdAndUpdate(_id,update);
+        res.send("Update Successfully");
+        
+    } catch (error) {
+        res.send("Error:"+error);
+    }
+})
+
 main()
     .then( ()=>{console.log("Connected to DB")
     app.listen(3000,()=>{
