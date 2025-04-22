1136c724448c099948981f9132e8280e438063c3
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-04-22 21:40:59 +0530

    updsoieuh

--- DsaPracticeProblem/NumberofEvenElementsfromStoE.cpp
+++ DsaPracticeProblem/NumberofEvenElementsfromStoE.cpp
@@ -1,13 +1,11 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 #define int long long
 #define endl '\n'
 const int MOD=1e9+7;
 const int INF=LLONG_MAX>>1;
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
 void solve(){
     int n;
     cin>>n;
