e3d89c28326757e35b15c01e628d55ca4783921a
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-04-23 13:49:41 +0530

    updarty

--- DsaPracticeProblem/NumberofEvenElementsfromStoE.cpp
+++ DsaPracticeProblem/NumberofEvenElementsfromStoE.cpp
@@ -1,11 +1,9 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 #define int long long
 #define endl '\n'
 const int MOD=1e9+7;
 const int INF=LLONG_MAX>>1;
-
 void solve(){
     int n;
     cin>>n;
