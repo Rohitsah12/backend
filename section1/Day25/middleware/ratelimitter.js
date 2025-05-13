811ecfc723d6e788f3488c82bf4fdd19f092c993
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-05-13 17:58:01 +0530

    fixed window rate limiter

--- section1/Day25/middleware/ratelimitter.js
+++ section1/Day25/middleware/ratelimitter.js
@@ -1,8 +1,15 @@
 const redisClient=require('../config/redis')
-const ratelimiter=async (req,resizeBy,next){
+const ratelimiter=async (req,resizeBy,next)=>{
     try{
         const ip=req.ip;
-        redisClient.incr(ip);
+        const count=await redisClient.incr(ip);
+        if(count==1){
+            await redisClient.expire(3600);
+        }
+        if(count>60){
+            throw new Error("User limit Excedded");
+        }
+        next();
     }
     catch(err){
         resizeBy.send("Error:",+err);
