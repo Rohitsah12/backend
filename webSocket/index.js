8013851c7ba6582b5b3a5189789e8f65729f085c
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-07-06 16:18:26 +0530

    feat: implement chat application with room functionality and message broadcasting

--- webSocket/index.js
+++ webSocket/index.js
@@ -1,28 +1,46 @@
 import express from 'express'
 import { Server } from 'socket.io';
-const app=express();
+const app = express();
+import path from 'path';
+import http from 'http'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Fix for __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // const server=app.listen(3000,()=>{
 //     console.log("Listening at port 3000");
 // })
 // const io=new Server(server);
 
-import http from 'http'
-const server=http.createServer(app);
-const io=new Server(server)
+const server = http.createServer(app);
+const io = new Server(server);
 
-io.on("connection",(socket)=>{
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    socket.on("message",(data)=>{
-        io.emit('new-mesage',data)
+io.on("connection", (socket) => {
+
+    // socket.on("message", (data) => {
+    //     socket.broadcast.emit('new-message', data);
+    // });
+
+    socket.on("message", ({room,msg}) => {
+        socket.to(room).emit('new-message',msg);
+    });
+
+    socket.on('join-room',(room)=>{
+        socket.join(room);
     })
 
-    socket.on("disconnect",()=>{
+    socket.on("disconnect", () => {
         console.log('dissconnected from server');
-        
-    })
-})
+    });
+});
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("Listening at port 3000");
-})
+});
