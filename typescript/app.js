84792bfe216aad68a559774adc3a92cead5ebf5f
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-07-29 00:40:35 +0530

    update

--- typescript/app.js
+++ typescript/app.js
@@ -2,3 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 10;
 let b = 20;
+let str = "Rohit";
+//boolean
+let isExist = true;
+isExist = false;
+let bigNumber = 13432123432123443212342n;
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,3 +1,12 @@
 let a:number=10
 let b:number=20
 
+
+let str:string="Rohit"
+
+//boolean
+let isExist:boolean=true;
+isExist=false;
+
+
+let bigNumber:bigint=13432123432123443212342n;
